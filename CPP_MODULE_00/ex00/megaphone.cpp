6911901b40c19f5e3437918d54fe49804bd23227
German <german.chernysh1@gmail.com>
2023-02-20 01:17:58 +0400

    start of cpp 00

---
+++ CPP_MODULE_00/ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gchernys <gchernys@42abudhabi.ae>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 00:59:29 by gchernys          #+#    #+#             */
+/*   Updated: 2023/02/20 01:15:18 by gchernys         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+int main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc <= 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	// j = 0;
+	// for (int i = 0; i < strlen(argv[j]); i++;)
+	// {
+	// 	if (islower(argv[j][i]))
+	// }
+}
