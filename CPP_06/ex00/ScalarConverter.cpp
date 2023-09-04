23e7903544995597d0eff88e727dae9c448d86d9
Germanos21 <89093952+Germanos21@users.noreply.github.com>
2023-09-04 00:40:23 +0000

    int almost finished

--- CPP_06/ex00/ScalarConverter.cpp
+++ CPP_06/ex00/ScalarConverter.cpp
@@ -7,16 +7,19 @@ static void convert_char(std::string input)
 		return ;
 
 	std::cout << "char: ";
-	int check = 0;
+	long check = 0;
 	std::istringstream str(input);
 
-	str >> check;
-
-	if (check == 0)
+	check = cpp_atoi_char(input);
+	if (check >= 0)
 	{
-		check = cpp_atoi_char(input);
+		check = 0;
+		str >> check;
 		if (check == 0)
+		{
         	std::cout << "impossible" << std::endl;
+			return ;
+		}
 	}
 
     if (check >= 0 && check < 32)
@@ -27,9 +30,28 @@ static void convert_char(std::string input)
         std::cout << "impossible" << std::endl;
 }
 
+static void	convert_int(std::string input)
+{
+	std::cout << "int: ";
+
+	const char *temp = input.data();
+	char *conv = const_cast<char*>(temp);
+	int check = 0;
+
+	if (check == 0)
+	{
+		check = cpp_atoi(conv);
+		if (check == 0)
+        	std::cout << "impossible" << std::endl;
+	}
+	std::cout << static_cast<int>(check) << std::endl;
+	return ;
+}
+
 void	ScalarConverter::Convert(std::string input)
 {
 	if (input.empty())
 		return ;
 	convert_char(input);
+	convert_int(input);
 }
